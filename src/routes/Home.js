469b43febb1b5fadbc8481c50266274727b72a91
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-11-29 17:19:46 -0500

    make url with parameter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -21,10 +21,11 @@ function Home() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div>  
           {movies.map((movie) => (
             <Movie
               key={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
